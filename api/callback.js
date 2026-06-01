ede4767dec2c50d5f335fa55ff7d7e2dc042913c
jaquesantos7-dotcom <jaquesantos7@gmail.com>
2026-06-01 17:50:19 -0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,9 +1,43 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
 
-    res.json({
-        clientId: process.env.ML_CLIENT_ID || null,
-        secretExists: !!process.env.ML_CLIENT_SECRET,
-        redirectUri: process.env.ML_REDIRECT_URI || null
-    });
+    const code = req.query.code;
+
+    if (!code) {
+        return res.status(400).json({
+            erro: "Código OAuth não recebido"
+        });
+    }
+
+    try {
+
+        const response = await fetch(
+            "https://api.mercadolibre.com/oauth/token",
+            {
+                method: "POST",
+                headers: {
+                    "accept": "application/json",
+                    "content-type": "application/x-www-form-urlencoded"
+                },
+                body: new URLSearchParams({
+                    grant_type: "authorization_code",
+                    client_id: process.env.ML_CLIENT_ID,
+                    client_secret: process.env.ML_CLIENT_SECRET,
+                    code: code,
+                    redirect_uri: process.env.ML_REDIRECT_URI
+                })
+            }
+        );
+
+        const data = await response.json();
+
+        return res.status(200).json(data);
+
+    } catch (erro) {
+
+        return res.status(500).json({
+            erro: erro.message
+        });
+
+    }
 
 }
