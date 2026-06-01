2b1a42f4f1a99965ea8bbfd3df293d218a6064b5
jaquesantos7-dotcom <jaquesantos7@gmail.com>
2026-06-01 18:22:23 -0300

    Update items.js

--- api/items.js
+++ api/items.js
@@ -2,38 +2,17 @@ export default async function handler(req, res) {
 
     const token = req.query.token;
 
-    try {
-
-        const userResponse = await fetch(
-            "https://api.mercadolibre.com/users/me",
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
+    const response = await fetch(
+        "https://api.mercadolibre.com/users/me",
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
             }
-        );
+        }
+    );
 
-        const user = await userResponse.json();
+    const data = await response.json();
 
-        const itemsResponse = await fetch(
-            `https://api.mercadolibre.com/users/${user.id}/items/search`,
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }
-        );
-
-        const items = await itemsResponse.json();
-
-        res.status(200).json(items);
-
-    } catch (erro) {
-
-        res.status(500).json({
-            erro: erro.message
-        });
-
-    }
+    res.status(200).json(data);
 
 }
