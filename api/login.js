3a4203081412b3e9bbb77e8850b109a1ae47de6c
jaquesantos7-dotcom <jaquesantos7@gmail.com>
2026-06-01 17:14:38 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,15 +1,11 @@
 export default function handler(req, res) {
 
-    const clientId = process.env.ML_CLIENT_ID;
+const url =
+    "https://auth.mercadolivre.com.br/authorization" +
+    "?response_type=code" +
+    "&client_id=4849212245586572" +
+    "&redirect_uri=https://ml-analytics-amber.vercel.app/api/callback";
 
-    const redirectUri =
-        "https://ml-analytics-amber.vercel.app/api/callback";
+res.redirect(url);
 
-    const url =
-        `https://auth.mercadolivre.com.br/authorization` +
-        `?response_type=code` +
-        `&client_id=${clientId}` +
-        `&redirect_uri=${redirectUri}`;
-
-    res.redirect(url);
 }
